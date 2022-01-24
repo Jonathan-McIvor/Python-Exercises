60395f45a6dd8eff43f8baa945e91d5df3d6074b
Jonathan McIvor <jonthan.mcivor99@gmail.com>
2022-01-24 17:46:51 +0000

    "Vowel or consonant identifier"

---
+++ ex-vowel-consonant-identifier.py
@@ -0,0 +1,20 @@
+# Create a new Python file and write a program that does the following:
+
+# # In this exercise you will create a program that reads a letter of the alphabet from the user
+# If the user enters a,e,i,o or u then your program should display a message indicating that the entered letter is a vowel
+# If the user enters y then your program should display a message indicating that sometimes y is a vowel, 
+# - and sometimes y is a consonant. Otherwise your program should display a message indicating that the letter is a consonant
+
+
+
+letter = input("Input letter: ")
+
+vowels = "aeiou"
+vac = "y"
+
+if letter in vowels not in vac:
+    print("The letter entered is a vowel")
+if letter in vac:
+    print("Sometimes y is a vowel, and sometimes y is a consonant")
+elif letter not in vowels not in vac:
+    print("Consonant")
